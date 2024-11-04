80b9237b567017259bfd093bfc39a98414ff47c6
Aadil1534 <159218202+Aadil1534@users.noreply.github.com>
2024-11-04 18:14:03 +0530

    Add files via upload

---
+++ conf.py
@@ -0,0 +1,7 @@
+SSID ="AC3d052a69fdbbd5cf7d08a69bf815ae17" #generated when you have created your twillio account"
+AUTH_TOKEN ="4196d1ff4f60e935f43eb29d1a715ce8" #you can access it from your twillio account dashboard
+FROM_NUMBER = "+12055827948"
+TO_NUMBER = "+919342289850" #must add +91 in the beginning
+
+BOLT_API = "1992e9b6-e563-467e-a606-41f8281cfb4a"
+DEVICE_ID ="BOLT15097703"
